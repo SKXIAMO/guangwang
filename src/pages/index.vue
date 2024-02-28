72455302c8d3827415f70425060e1f67d47c0e46
NSCHAO <2069571638@qq.com>
2024-02-28 15:40:02 +0800

    修改背景

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,15 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="" />
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 p-2 p-x-5 text-5 color-white class="bg-#B3E0AF">
+      <div rd-2 p-2 p-x-5 text-5 color-white class="text-bg-color">
         Nikhilkashyap34@icloud.com
       </div>
     </div>
   </div>
 </template>
+
+<style>
+.text-bg-color {
+  background-color: #b3e0af;
+}
+</style>
