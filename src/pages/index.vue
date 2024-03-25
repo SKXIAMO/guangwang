7531e91ee89408471d22de818494d73c2afb0108
NSCHAO <2069571638@qq.com>
2024-03-25 17:44:31 +0800

    2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,13 +9,16 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="" />
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-6 color-white class="delish_bg_color">bullincappetta445@gmail.com</div>
+      <div rd-2 bg-black p-2 p-x-5 text-6 class="delish_bg_color">
+        bullincappetta445@gmail.com
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 .delish_bg_color {
-  background-color: #daf181;
+  background-color: black;
+  color: white;
 }
 </style>
