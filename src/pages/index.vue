ec42882f8f08596e591d88a8633a7f270bb67c6f
NSCHAO <2069571638@qq.com>
2024-03-25 17:39:36 +0800

    改邮箱

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts" generic="T extends any, O extends any">
 defineOptions({
-  name: 'IndexPage',
-})
+  name: "IndexPage",
+});
 </script>
 
 <template>
   <div relative min-h-100vh>
-    <img h-full w-full src="../assets/bg.webp" alt="">
+    <img h-full w-full src="../assets/bg.webp" alt="" />
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
-      </div>
+      <div rd-2 bg-black p-2 p-x-5 text-6 color-white class="delish_bg_color">bullincappetta445@gmail.com</div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.delish_bg_color {
+  background-color: #daf181;
+}
+</style>
