f8bd2b0ebb98391ce0208502f9ea5ec8fe589f57
NSCHAO <2069571638@qq.com>
2024-04-02 11:12:01 +0800

    正式官网

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts" generic="T extends any, O extends any">
 defineOptions({
-  name: 'IndexPage',
-})
+  name: "IndexPage",
+});
 </script>
 
 <template>
   <div relative min-h-100vh>
-    <img h-full w-full src="../assets/bg.webp" alt="">
+    <img h-full w-full src="../assets/bg.webp" alt="" />
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
-      </div>
+      <div rd-2 p-2 p-x-5 text-7 class="probe-yx">kunkacinnamon872@gmail.com</div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.probe-yx {
+  color: #000;
+  background-color: #fff;
+  border: 1px solid #000;
+  border-radius: 10px;
+}
+</style>
