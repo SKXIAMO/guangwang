99a2401df9ad181d8666774dbcb97d9624f5f6ac
NSCHAO <2069571638@qq.com>
2024-04-03 16:46:32 +0800

    修改4.3

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,13 +9,13 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="" />
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 p-2 p-x-5 text-7 class="probe-yx">kunkacinnamon872@gmail.com</div>
+      <div rd-2 p-2 p-x-5 text-7 class="Flurv-yx">kunkacinnamon872@gmail.com</div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.probe-yx {
+.Flurv-yx {
   color: #000;
   background-color: #fff;
   border: 1px solid #000;
--- src/pages/users.vue
+++ src/pages/users.vue
@@ -2,7 +2,7 @@
 defineOptions({
   name: 'IndexPage',
 })
-const appName = 'Probe'
+const appName = 'Flurv'
 </script>
 
 <template>
