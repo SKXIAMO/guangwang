5b85ddb78c760fa1baacdb070414163916a76b25
NSCHAO <2069571638@qq.com>
2024-05-14 19:37:18 +0800

    改app名字图标

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,7 +8,7 @@ defineOptions({
   <div relative min-h-100vh>
     <img h-full w-full src="../assets/bg.png" alt="">
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white exited>
+      <div rd-2 bg-black p-2 p-x-5 color-white exited>
         rundahlwartman@gmail.com
       </div>
     </div>
@@ -17,7 +17,7 @@ defineOptions({
 
 <style scoped>
 .exited{
-  background-color: #fff;
+  background-color: #CEFA69;
   color: black;
   font-size: 30px;
 }
