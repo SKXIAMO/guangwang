c208e9dfdef1b9b4796a00c937f04456eef90594
SKXIAMO <2069571638@qq.com>
2024-05-16 01:22:42 -0700

    改正式

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,12 +6,20 @@ defineOptions({
 
 <template>
   <div relative min-h-100vh>
-    <img h-full w-full src="../assets/bg.webp" alt="">
+    <img h-full w-full src="../assets/bg.png" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2 p-2 p-x-5 text-8 color-white unipets>
+        viviennestropus@gmail.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.unipets{
+  background-color: #F1972E;
+  color: #ffffff;
+  font-size: 30px;
+}
+</style>
