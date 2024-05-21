b03618bcfba84ce9020b57fc10a252cfd3d5153d
SKXIAMO <2069571638@qq.com>
2024-05-21 00:59:26 -0700

    上包

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,12 +6,19 @@ defineOptions({
 
 <template>
   <div relative min-h-100vh>
-    <img h-full w-full src="../assets/bg.webp" alt="">
+    <img h-full w-full src="../assets/bg.png" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2 p-2 p-x-5 text-8 cycling>
+        azorechiappetta@gmail.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.cycling{
+  background-color: #F3C844;
+  color: black;
+}
+</style>
--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -2,7 +2,7 @@
 defineOptions({
   name: 'IndexPage',
 })
-const appName = 'BMX'
+const appName = 'ilike'
 </script>
 
 <template>
