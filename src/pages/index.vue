8403dc54de1aa9d67d4a4c9587778e07f895bc78
SKXIAMO <2069571638@qq.com>
2024-05-30 03:01:25 -0700

    new

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,19 +6,17 @@ defineOptions({
 
 <template>
   <div relative min-h-100vh>
-    <img h-full w-full src="../assets/bg.png" alt="" />
+    <img h-full w-full src="../assets/bg.webp" alt="" />
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 p-2 p-x-5 text-7 class="TOEKExpeditionElevate-yx">kunkacinnamon872@gmail.com</div>
+      <div rd-2 p-2 p-x-5 text-7 class="Riddle-yx">kunkacinnamon872@gmail.com</div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.TOEKExpeditionElevate-yx {
-  color: #000;
-  background-color: #fff;
-  border: 1px solid #000;
-  border-radius: 10px;
+.Riddle-yx {
+  color: #fff;
+  background-color: #000;
 }
 </style>
--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -2,7 +2,7 @@
 defineOptions({
   name: 'IndexPage',
 })
-const appName = 'TOEKExpeditionElevate'
+const appName = 'Riddle'
 </script>
 
 <template>
