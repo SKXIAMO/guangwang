aa0f7a85f8b6441a30219ce87101f672d00f3e98
SKXIAMO <2069571638@qq.com>
2024-06-06 19:57:58 -0700

    正式邮箱

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,16 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2  p-2 p-x-5 text-10 class="glow_up">
+        leveregaertner@gmail.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.glow_up{
+  background-color: #FDEAEE;
+  color: black;
+}
+</style>
