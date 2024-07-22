0c27ac0ba38e69d5a28b554f08f155e3c9aac42e
SKXIAMO <2069571638@qq.com>
2024-07-22 02:29:06 -0700

    new

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,17 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
+      <div rd-2 bg-black p-2 p-x-5 text-8 color-white class="leep">
         639293382@qq.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.leep{
+  background-color: #FFA938;
+  border-radius: 20px;
+  color: black;
+}
+</style>
