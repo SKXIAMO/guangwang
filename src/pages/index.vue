15d31d33454c66e382caef12b4dd2007f498449e
SKXIAMO <2069571638@qq.com>
2024-07-24 01:04:19 -0700

    邮箱

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,18 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2 bg-black p-2 p-x-5 text-8 color-white class="conn">
+        niweijie2626@icloud.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.conn{
+  background-color: #FB8DB6;
+  border-radius: 0px;
+  color: black;
+  border: 2px solid #000;
+}
+</style>
