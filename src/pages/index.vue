206837be7e4c3b255a93e4391788f95ba651eb18
SKXIAMO <2069571638@qq.com>
2024-08-08 23:13:09 -0700

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,16 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2 class="talkzy" p-2 p-x-5 text-8 color-white>
+        xuwenjuan0416@icloud.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.talkzy{
+  background-image: linear-gradient(to right, #1472FF,#80B3FF);
+  border-radius: 12px;
+}
+</style>
