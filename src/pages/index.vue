89c67cd175452f480c2091c06409452329e07688
SKXIAMO <2069571638@qq.com>
2024-08-14 20:14:18 -0700

    new

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,16 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2  p-2 p-x-5 text-8  class="cose">
+        cqy30298567868@icloud.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.cose{
+  background-color: #80B3FF;
+  color: #fff;
+}
+</style>
