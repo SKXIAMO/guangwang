cf12e28d75a3f27bc051fd69002f2eeed3634fdf
SKXIAMO <2069571638@qq.com>
2024-10-25 00:11:40 -0700

    邮箱

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,15 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2 class="jamx" p-2 p-x-5 text-8 color-white>
+        bunanalu123@icloud.com
       </div>
     </div>
   </div>
 </template>
+
+<style lang="less" scoped>
+.jamx{
+  background-color: #14FBFF;
+}
+</style>
