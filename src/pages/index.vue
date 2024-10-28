d3f264fc3eaee72c62ecb9fcf295c33c8f9d36b9
SKXIAMO <2069571638@qq.com>
2024-10-27 20:35:05 -0700

    改名

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,7 +9,7 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 class="jamx" p-2 p-x-5 text-8 color-white>
+      <div rd-2 class="Cahet" p-2 p-x-5 text-8 color-white>
         bunanalu123@icloud.com
       </div>
     </div>
@@ -17,7 +17,7 @@ defineOptions({
 </template>
 
 <style scoped>
-.jamx{
+.Cahet{
   background-color: #14FBFF;
 }
 </style>
--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -2,7 +2,7 @@
 defineOptions({
   name: 'IndexPage',
 })
-const appName = 'Jamx'
+const appName = 'Cahet'
 </script>
 
 <template>
