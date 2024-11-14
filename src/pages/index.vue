e14efa0be7d0412332b3e48b6e10d62cbeca24bb
SKXIAMO <2069571638@qq.com>
2024-11-13 23:39:47 -0800

    邮箱

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,18 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2 class="baila" p-2 p-x-5 text-8 color-white>
+        heyu20000408@icloud.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.baila{
+  background-color: #fff;
+  color: #FA3CB3;
+  border: 1px solid #FA3CB3;
+  border-radius: 10px;
+}
+</style>
