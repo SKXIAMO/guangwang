f6ae7cea1a7ae3d7d40e4ef29972b38a73f75c14
SKXIAMO <2069571638@qq.com>
2024-11-26 20:35:16 -0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,16 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2 p-2 p-x-5 text-8 class="diive">
+        chenzhiqiang20021004@icloud.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.diive{
+  background-color: #fff;
+  color: #000;
+}
+</style>
