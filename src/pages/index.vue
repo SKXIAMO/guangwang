63abdcddccefda51491e618e36e92a32540e132b
SKXIAMO <2069571638@qq.com>
2024-12-26 19:50:43 -0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,16 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2 p-2 p-x-5 text-8 class="vartx">
+        huangchunyan20020312@icloud.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.vartx{
+  background-color: #E13CED;
+  color: #fff;
+}
+</style>
