b2f7c94cf46e6d89a7425d279a49f47754a9e693
SKXIAMO <2069571638@qq.com>
2025-03-04 19:34:33 -0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,10 +8,17 @@ defineOptions({
   <div relative min-h-100vh>
     <img h-full w-full src="../assets/bg.webp" alt="">
 
-    <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+    <div class="email" absolute top-10 w-full flex justify-center>
+      <div rd-2 p-2 p-x-5 text-8 class="cl">
+        donutchatlive@gmail.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.cl {
+  background-color: #fff;
+  color: #000;
+}
+</style>
